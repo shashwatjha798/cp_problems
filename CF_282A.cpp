432ccaf6eaaf7092e5bcf069a502a10a683cd57e
Shashwat Jha <57710981+shashwatjha798@users.noreply.github.com>
2021-05-18 19:06:29 +0530

    Add files via upload
    Problem: https://codeforces.com/problemset/problem/282/A

---
+++ CF_282A.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include<strings.h>
+using namespace std;
+int main() 
+{
+	int t;
+	cin>>t;
+	
+	int x = 0;
+	string s;
+	
+	while(t)
+	{
+		cin>>s;
+		if(s[0] == '+' || s[2] == '+')
+			x = x+1;
+		
+		else if (s[0] == '-' || s[2] == '-')
+			x = x-1;
+		
+		t--;
+	}
+	
+	cout<<x;
+	
+	return 0;
+}
