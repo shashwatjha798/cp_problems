71f01ef3c7e086d55254912793eee774cd3dc911
Shashwat Jha <57710981+shashwatjha798@users.noreply.github.com>
2021-05-20 15:30:37 +0530

    Add files via upload

---
+++ Codeforces/Beautiful Matrix.cpp
@@ -0,0 +1,19 @@
+//https://codeforces.com/problemset/problem/263/A
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	
+	int a[5][5];
+	
+	for(int i = 0; i<5; ++i){
+		for(int j = 0; j<5; ++j){
+			cin>>a[i][j];
+			if(a[i][j] == 1){
+				cout<<abs(i+1-3) + abs(j+1-3);
+				break;
+			}
+		}
+	}
+	return 0;
+}
