3465a5aafdf09451f55085c6059203821e1e5b7f
Shashwat Jha <57710981+shashwatjha798@users.noreply.github.com>
2021-06-28 21:43:32 +0530

    Add files via upload

---
+++ AtCoder/Repression.cpp
@@ -0,0 +1,13 @@
+//https://atcoder.jp/contests/abc207/tasks/abc207_a
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	
+	int a[3];
+	cin>>a[0]>>a[1]>>a[2];
+	sort(a, a+3);
+	cout<<(a[1]+a[2]);
+	
+	return 0;
+}
