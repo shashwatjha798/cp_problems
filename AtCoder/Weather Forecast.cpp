8e44893180482da6238654bdea7ba0f44b54bd07
Shashwat Jha <57710981+shashwatjha798@users.noreply.github.com>
2021-09-11 22:09:18 +0530

    Add files via upload

---
+++ AtCoder/Weather Forecast.cpp
@@ -0,0 +1,13 @@
+//https://atcoder.jp/contests/abc218/tasks/abc218_b
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	for(int i = 0; i<26; ++i){
+		int n; cin>>n;
+		
+		cout<<char('a'+n-1);
+	}
+	return 0;
+}}
