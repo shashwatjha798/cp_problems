4ff3a0f4c450b9e73a064969fa270cfde5eafb49
Shashwat Jha <57710981+shashwatjha798@users.noreply.github.com>
2021-09-18 15:05:12 +0530

    Add files via upload

---
+++ Codeforces/Ultra-Fast Mathematician.cpp
@@ -0,0 +1,15 @@
+//https://codeforces.com/problemset/problem/61/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	string a , b;
+	cin>>a>>b;
+	
+	for(int i = 0; i<a.size(); ++i){
+		if(a[i] != b[i]) cout<<1;
+		else cout<<0; 
+	}
+	return 0;
+}
